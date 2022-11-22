052d2264ceda19400b60d45cd6769aefe75d5b3b
pbg0823 <79179430+pbg0823@users.noreply.github.com>
2022-11-22 16:54:04 +0900

    Delete ItemService directory

--- ItemService/src/main/java/com/submarket/itemservice/service/ItemReviewCheckService.java
+++
@@ -1,7 +0,0 @@
-package com.submarket.itemservice.service;
-
-import com.submarket.itemservice.dto.ItemReviewDto;
-
-public interface ItemReviewCheckService {
-    boolean canCreateReview(ItemReviewDto itemReviewDto, int itemSeq) throws Exception;
-}
--- ItemService/src/main/java/com/submarket/itemservice/service/KafkaProducerService.java
+++
@@ -1,8 +0,0 @@
-package com.submarket.itemservice.service;
-
-import com.submarket.itemservice.dto.ItemDto;
-
-public interface KafkaProducerService {
-    void sendItemInfoToOrderService(ItemDto itemDto) throws Exception;
-
-}
