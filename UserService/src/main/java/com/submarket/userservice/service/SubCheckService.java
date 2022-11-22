aaadc87960a6b9eba6e8b0fcba083c8e2a747734
pbg0823 <79179430+pbg0823@users.noreply.github.com>
2022-11-22 16:44:07 +0900

    Delete UserService directory

--- UserService/src/main/java/com/submarket/userservice/service/SubCheckService.java
+++
@@ -1,7 +0,0 @@
-package com.submarket.userservice.service;
-
-public interface SubCheckService {
-    boolean SubCheck(Integer subSeq) throws Exception;
-
-    boolean checkHasSubByItemSeqAndUserId(Integer itemSeq, String userId) throws Exception;
-}
