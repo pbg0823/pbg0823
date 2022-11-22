a3d92a157ba4246d288938f128ab2c36923804ec
pbg0823 <79179430+pbg0823@users.noreply.github.com>
2022-11-22 16:54:23 +0900

    Delete SellerService directory

--- SellerService/src/main/java/com/submarket/sellerservice/service/IKafkaConsumerService.java
+++
@@ -1,5 +0,0 @@
-package com.submarket.sellerservice.service;
-
-public interface IKafkaConsumerService {
-    void getOrderInfoFromOrderService(String kafkaMessage) throws Exception;
-}
--- SellerService/src/main/java/com/submarket/sellerservice/service/IKafkaProducerService.java
+++
@@ -1,7 +0,0 @@
-package com.submarket.sellerservice.service;
-
-import com.submarket.sellerservice.dto.SellerDto;
-
-public interface IKafkaProducerService {
-    void kafkaDeleteSeller(SellerDto sellerDto) throws Exception;
-}
